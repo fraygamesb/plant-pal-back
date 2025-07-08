28dece925dc845c9b47ffa7246a6378eff4ae8f3
fraygamesb <aziablov@sdvor.com>
2025-07-08 11:56:17 +0500

    Добавил в модель поле created_at, убрал поле phone_number

--- src/core/infrastructure/db/models/user.py
+++ src/core/infrastructure/db/models/user.py
@@ -1,9 +1,9 @@
 import uuid
+from datetime import datetime
 
-from pydantic.v1 import EmailStr
-from sqlalchemy import String, Text, DateTime, func, Integer, Float, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
+
 
 class User(Base):
     __tablename__ = "users"
@@ -11,7 +11,8 @@ class User(Base):
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
 
     name: Mapped[str]
-    email: Mapped[EmailStr]
+    email: Mapped[str]
     password: Mapped[str]
-    phone_number: Mapped[str]
     is_active: Mapped[bool] = False
+    created_at: Mapped[datetime]
+    # phone_number: Mapped[str]
