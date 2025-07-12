32b2fb00b1ab293aa3c2b79830b5cc5ab981be23
fraygamesb <aziablov@sdvor.com>
2025-07-13 03:11:54 +0500

    дописал crud для user

--- src/core/repositories/user.py
+++ src/core/repositories/user.py
@@ -20,11 +20,15 @@ class UserRepository:
         self.session = session
         self.model = User
 
+    async def get_by_id(self, id: str) -> User | None:
+        result = await self.session.execute(select(User).where(User.id == id))
+        return result.scalar_one_or_none()
+
     async def get_by_email(self, email: str) -> User | None:
         result = await self.session.execute(select(User).where(User.email == email))
         return result.scalar_one_or_none()
 
-    async def get_by_username(self, name: str) -> User | None:
+    async def get_by_name(self, name: str) -> User | None:
         result = await self.session.execute(select(User).where(User.name == name))
         return result.scalar_one_or_none()
 
@@ -45,3 +49,65 @@ class UserRepository:
         await self.session.commit()
         await self.session.refresh(user)
         return user
+
+    async def update(
+        self, user_id: uuid.UUID, user_data: UserUpdateRequestSchema
+    ) -> User | None:
+        user = await self.get_by_id(str(user_id))
+        if not user:
+            return None
+
+        update_data = {}
+
+        if user_data.name is not None:
+            update_data["name"] = user_data.name
+
+        if user_data.email is not None:
+            update_data["email"] = user_data.email
+
+        if user_data.password is not None:
+            update_data["password"] = hash_password(user_data.password).decode()
+
+        if update_data:
+            await self.session.execute(
+                update(User).where(User.id == user_id).values(**update_data)
+            )
+            await self.session.commit()
+
+            await self.session.refresh(user)
+
+        return user
+
+    async def delete(self, user_id: uuid.UUID) -> bool:
+        user = await self.get_by_id(str(user_id))
+        if not user:
+            return False
+
+        await self.session.execute(delete(User).where(User.id == user_id))
+        await self.session.commit()
+
+        return True
+
+    async def deactivate(self, user_id: uuid.UUID) -> bool:
+        user = await self.get_by_id(str(user_id))
+        if not user:
+            return False
+
+        await self.session.execute(
+            update(User).where(User.id == user_id).values(is_active=False)
+        )
+        await self.session.commit()
+
+        return True
+
+    async def activate(self, user_id: uuid.UUID) -> bool:
+        user = await self.get_by_id(str(user_id))
+        if not user:
+            return False
+
+        await self.session.execute(
+            update(User).where(User.id == user_id).values(is_active=True)
+        )
+        await self.session.commit()
+
+        return True
