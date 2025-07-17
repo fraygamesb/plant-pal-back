310191eaafc144f8759717626795e4834e52415d
fraygamesb <aziablov@sdvor.com>
2025-07-17 13:20:23 +0500

    добавил проверку на дубликат email при регистрации

--- src/core/api/v1/endpoints/auth/register.py
+++ src/core/api/v1/endpoints/auth/register.py
@@ -11,8 +11,11 @@ router = APIRouter(prefix="/register", tags=["Регистрация польз�
 
 @router.post("", response_model=UserResponseSchema, status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreateRequestSchema, repo: UserRepository = Depends()):
-    existing = await repo.get_by_name(user.name)
-    if existing:
+    existing_name = await repo.get_by_name(user.name)
+    existing_email = await repo.get_by_email(user.email)
+    if existing_name:
         raise HTTPException(status_code=400, detail="Username already registered")
+    if existing_email:
+        raise HTTPException(status_code=400, detail="Email already registered")
     db_user = await repo.create(user)
     return db_user
